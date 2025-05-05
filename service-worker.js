a96c9d0d776bb80e62717369893db712f523a9df
fishr-flash <fishr-flash@users.noreply.github.com>
2025-05-05 08:45:42 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,13 @@
+self.addEventListener('install', event => {
+  console.log('[SW] Installed');
+});
 
+self.addEventListener('activate', event => {
+  console.log('[SW] Activated');
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => new Response('Offline content'))
+  );
+});
